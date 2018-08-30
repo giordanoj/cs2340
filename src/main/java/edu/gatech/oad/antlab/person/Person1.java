e0db7857285c81e9c90fda855a213b8e68fd0c2d
john giordano <johnfg1@gmail.com>
2018-08-30 18:51:40 -0400

    giordano modification

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author John Giordano
  *  @version 1.1
  */
 public class Person1 {
@@ -31,7 +31,7 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+	  return input.substring(2) + input.substring(0,2);
 	}
 	
 	/**
